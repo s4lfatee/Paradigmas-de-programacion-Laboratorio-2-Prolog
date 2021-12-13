0dd864a559a8c44929007965cc333aded7cc5111
Francisco Salfate <francisco.salfate.ga@usach.cl>
2021-12-13 09:44:08 -0300

    TDA Usuario y Documento
    Se crean los TDAs Usuario y Documento, para ambos se implementa un constructor y sus selectores.

--- paradigmadocs_20847783_SalfateGarces.pl
+++ paradigmadocs_20847783_SalfateGarces.pl
@@ -5,20 +5,22 @@
 % de tipo entero, un mes de tipo entero, y un anho de tipo entero.
 
 % Constructor
-fecha(D,M,Y, [D,M,Y]):-
-            number(D),number(M),number(Y),
-            M > 0, (M < 12; M = 12).
+fecha(Dia,Mes,Year, [Dia,Mes,Year]):-
+            number(Dia),number(Mes),number(Year),
+            Mes > 0, (Mes < 12; Mes = 12).
 
 % Selectores
-getdia([DIA, _, _], DIA).
+getdia([Dia, _, _], Dia).
 
-getmes([_, MES, _], MES).
+getmes([_, Mes, _], Mes).
 
-getanho([_, _, ANHO], ANHO).
+getanho([_, _, Year], Year).
 
 % Pertenencia
-isfecha([D,M,Y]) :-
-    fecha(D,M,Y,_).
+isfecha([Dia,Mes,Year]) :-
+    fecha(Dia,Mes,Year,_).
+
+
 
 
 % TDA Paradigmadocs
@@ -39,3 +41,49 @@ getlistausers([_,_,Listausers,_], Listausers).
 
 getlistadocs([_,_,_,Listadocs], Listadocs).
 
+
+
+
+% TDA Usuario
+% El TDA Usuario se representa a traves de una lista que almacena el
+% nombre de un usuario (string), su contrasenha (string) y su fecha de
+% registro (tipo fecha)
+
+% Constructor
+usuario(Username, Password, UserDate, [Username, Password, UserDate]).
+
+
+% Selectores
+getusername([Username,_,_], Username).
+
+getpassword([_, Password,_], Password).
+
+getUserDate([_,_, UserDate], UserDate).
+
+
+
+
+% TDA Documento
+% El TDA Documento se representa a traves de una lista que almacena el
+% titulo de un documento (String), su fecha de creacion (tipo fecha),
+% contenido (string), un valor de identificacion (entero), el duenho del
+% documento y dos listas para los accesos del documento y su historial
+% de versiones.
+
+% Constructor
+documento(Titulo, FechaDoc, Contenido, Id, Owner, [Titulo, FechaDoc, Contenido, Id, Owner, [], []]).
+
+% Selectores
+gettitulo([Titulo, _, _, _, _, _, _], Titulo).
+
+getfechaDoc([_, FechaDoc, _, _, _, _, _], FechaDoc).
+
+getContenido([_, _, Contenido, _, _, _, _], Contenido).
+
+getIdDoc([_, _, _, Iddoc, _, _, _], Iddoc).
+
+getOwner([_, _, _, _, Owner, _, _], Owner).
+
+getAccessList([_, _, _, _, _ , AccessList, _], AccessList).
+
+getVersionList([_, _, _, _, _, _, VersionList], VersionList).
